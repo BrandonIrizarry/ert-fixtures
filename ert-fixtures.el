f65f6775a04dfc5960f4b27c9c2d18e0b9bba425
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2023-11-06 14:43:36 -0500

    Expand on docstring

--- ert-fixtures.el
+++ ert-fixtures.el
@@ -1,4 +1,5 @@
 ;; -*- lexical-binding: t; -*-
+
 (defun efs--officialize-macro-names ()
   "Give syntax highlighting to the names of macros."
   ;; This code is adapted from the ERT source.
@@ -26,9 +27,21 @@ common fixture for those tests:
 (defvar origin-fixture (ert-fixtures-define-fixture ((x 0) (y 0)))
   \"A fixture that uses the origin as its main point.\")
 
-You'd then use the macro 'ert-fixtures-use-fixture' to apply this
-fixture to the test code you'd otherwise write with
-'ert-deftest':
+ORIGIN-FIXTURE is a lambda that takes in test code as an
+argument (in the form of a function), where x and y will be
+defined via dynamic scope. That is, the test code already expects
+to use these variables. Example use:
+
+(ert-deftest origin-test-1 ()
+  (funcall origin-fixture
+           (lambda ()
+              (should (zerop x))
+              (should (zerop y)))))
+
+Since this still looks a bit artifical, the convenience macro
+'ert-fixtures-use-fixture' is provided to let you apply this
+fixture directly to your test code, setting up the required ERT
+test for you:
 
 (ert-fixtures-use-fixture origin-test-1 (origin-fixture)
   (should (zerop x))
